eaa33509d63343873f95758dc0fcd9f880d16157
jipsy <jean-philippe.riviere@lri.fr>
2018-03-29 13:27:18 +0200

    Sensors myo ok

--- src/routes/sensors2.js
+++ src/routes/sensors2.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
 let smartphoneSensors = require('../../public/js/smartphoneSensors');
+let myo = require('../../public/js/myo');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   console.log('Accessing to sensors page');
-  res.render('sensors', { title: 'sensor for testing sensors MYO: '});
+  var Myo = require('myo');
+  
+  Myo.connect('com.stolksdorf.myAwesomeApp', require('ws'));
+  
+  Myo.on('fist', function(){
+    console.log('Hello Myo!');
+    //this.vibrate();
+  });
+  
+  
+  res.render('sensors2', { title: 'sensor for testing sensors MYO: '});
+  
+  
   //next(); // pass control to the next handler
 });
 
